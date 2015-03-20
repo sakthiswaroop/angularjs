9f8da5292ef93653e4f5c759c8a4422f5a98aabc
Vasudhaika <nitbss@gmail.com>
2015-03-20 10:26:04 +0530

    done

---
+++ practise/controller.js
@@ -0,0 +1,3 @@
+function firstctrl($scope){
+
+}
---
+++ practise/praticejs.js
@@ -0,0 +1,20 @@
+var firstapp = angular.module('firstapp', []);	
+
+firstapp.controller('firstCtrl', ['$scope', function ($scope) {
+
+
+}]);
+var secondapp = angular.module('secondCtrl', [])
+
+secondapp.controller('secondCtrl', ['$scope', function ($scope) {
+
+
+}]);
+
+
+ 
+	function f()
+	{
+
+		alert('working');
+	}
